158a48b87b9bfc99d7753e45ed7689f454b17870
jprock23 <juanproque@outlook.com>
2024-03-05 16:22:03 -0600

    Formalized autos

--- src/main/java/frc/robot/subsystems/led/LED.java
+++ src/main/java/frc/robot/subsystems/led/LED.java
@@ -1,8 +1,5 @@
 package frc.robot.subsystems.led;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import frc.robot.Ports;
 
@@ -10,23 +7,11 @@ public class LED {
 
     private static LED instance;
 
-    private AddressableLED lights;
-    private AddressableLEDBuffer buffer;
-
-    private static final int length = 0;
-
     private Spark PWMLED;
 
     public LED() {
 
         PWMLED = new Spark(Ports.blinkin);
-
-        // lights = new AddressableLED(Ports.blinkin);
-        // buffer = new AddressableLEDBuffer(length);
-
-        // lights.setLength(length);
-
-        // lights.setData(buffer);
     }
 
     public void setBlue(){
@@ -48,32 +33,6 @@ public class LED {
     public void setRed() {
         PWMLED.set(-.35);
     }
-
-    // public void start() {
-    // lights.start();
-    // }
-
-    // public void setData(){
-    // lights.setData(buffer);
-    // }
-
-    // public void setRed() {
-    // for (int i = 0; i < length; i++) {
-    // buffer.setRGB(i, 255, 0, 0);
-    // }
-    // }
-
-    // public void setGreen() {
-    // for (int i = 0; i < length; i++) {
-    // buffer.setRGB(i, 0, 255, 0);
-    // }
-    // }
-
-    // public void setBlue() {
-    // for (int i = 0; i < length; i++) {
-    // buffer.setRGB(i, 255, 0, 255);
-    // }
-    // }
 
     public static LED getInstance() {
         if (instance == null) {
